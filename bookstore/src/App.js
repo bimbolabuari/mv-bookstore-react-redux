7a637c42831bfeac193d0ffa3f8856dacdc174c6
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-08-24 16:26:45 +0100

    feat: Edit route path in App.js

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from './components/header';
 import Categories from './redux/categories/categories';
 import Books from './components/books';
-import DisplayBooks from './components/displayBook';
+
 
  const App = () => {
     return (
@@ -12,7 +12,7 @@ import DisplayBooks from './components/displayBook';
           <div>
           <Switch>
              <Route exact path="/">
-             <DisplayBooks />
+             <Books />
              </Route>
             <Route path="/books">
               <Books />
