0eb02b43eabda5dbde10088cff2d4650743909ac
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-08-26 23:02:59 +0100

    feat: Add form component

--- src/components/createNewBook.js
+++ src/components/createNewBook.js
@@ -1,28 +1,56 @@
 import React from 'react';
+import { useState } from 'react';
 import PropTypes from "prop-types";
+import { connect } from 'react-redux';
+import { addBook } from '../actions/bookActionCreator';
 
-const CreateNewBook = (props) => {
-    return (
-    <div>
-        <h2>Add new Book</h2>
-        <form>
-            <input type="text" placeholder="Book title" />
-            <input type="text" placeholder="Book Author" />
-            <select id={props.id} name={props.name}>
-            <option>Science Fiction</option>
-            <option>Economy</option>
-            <option>Action</option>
-            <option>Documentaries</option>
-            </select>
-            <button>Add Book</button>
-        </form>
-    </div>
-    )
+    const CreateNewBook = ({addBook}) => {
+      const [title, setTitle] = useState('');
+      const [author, setAuthor] = useState('');
+      const [genre, setGenre] = useState('Science Fiction');
+    
+    
+      const handleTitleChange = (event) => {
+        setTitle(event.target.value)
+      }
+      const handleAuthorChange = (event) =>  {
+        setAuthor(event.target.value)
+      }
+
+      const handleTypeChange = (event) => {
+        setGenre(event.target.value)
+      }
+
+      
+      const handleSubmit = (event) => {
+        event.preventDefault()
+        addBook({title, author, genre})
+        return false
+      }
+        return (
+        <div>
+            <h2>Add new Book</h2>
+            <form>
+                <input type="text" placeholder="Book title" value={title} onChange={handleTitleChange}  required/>
+                <input type="text" placeholder="Book Author" value={author} onChange={handleAuthorChange} required/>
+                <select  onChange={handleTypeChange}>
+                <option>Science Fiction</option>
+                <option>Economy</option>
+                <option>Action</option>
+                <option>Documentaries</option>
+                </select>
+                <button type='button' onClick={event => handleSubmit(event)}>Add Book</button>
+            </form>
+        </div>
+        );
+      
 };
+
 
 CreateNewBook.propTypes = {
     id: PropTypes.string,
-    name: PropTypes.string
+    name: PropTypes.string,
+    addBook: PropTypes.func
   };
 
-export default CreateNewBook;
+  export default connect(state => state, {addBook})(CreateNewBook);
