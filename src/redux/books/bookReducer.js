ed6d8f16fda7a188806c215601979847c24c4769
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-08-26 23:04:31 +0100

    feat: Add book reducer

--- src/redux/books/bookReducer.js
+++ src/redux/books/bookReducer.js
@@ -1,12 +1,23 @@
-import * as Actions from './bookAction';
+import * as actions from '../../actions/bookAction';
+import { v4 as uuidv4 } from 'uuid';
 
-const bookReducer = (state = {}, action = {}) => {
-    switch (action.type) {
-      case Actions.BOOK_ADDED:
-      case Actions.BOOK_REMOVED:
-      case Actions.BOOK_DISPLAYED:
+export const initialBooks = [
+  {type: 'Science fiction', title: 'Book One', author: "author", id: uuidv4()},
+  {type: 'Economy', title: 'Book Two', author: "author", id: uuidv4()},
+  {type: 'Documentation', title: 'Book Three', author: "author", id: uuidv4()}
+]
+
+const books =  (state = initialBooks, action) => {
+      switch (action.type) {
+        case actions.BOOK_ADDED:
+          const {id, title, author, genre} = action
+          return [
+            ...state, {id, title, author, genre}
+          ];
+      case actions.BOOK_REMOVED:
+        return state.filter(book => book.id !== action.id);
       default: return state;
     }
-  }
+};
 
-  export default bookReducer;
+export default books;
