87bdbb3255925e8c60ea4f11f939858bc12d68a6
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-08-27 14:10:09 +0100

    feat: Create action function to getbook action

--- src/actions/bookActionCreator.js
+++ src/actions/bookActionCreator.js
@@ -1,15 +1,19 @@
 import * as actions from '../actions/bookAction';
 import { v4 as uuidv4 } from 'uuid';
 
-export const addBook = ({title, author, genre}) => ({
+export const addBook = ({title, category}) => ({
     type: actions.BOOK_ADDED,
     id: uuidv4(),
    title,
-   author,
-   genre
+   category
 })
 
 export const removeBook = (id) => ({
     type: actions.BOOK_REMOVED,
-    id
+    id,
 })
+
+export const getBook = (payload) => ({
+    type: actions.BOOK_GOTTEN,
+    payload,
+  });
