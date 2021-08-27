ce8a67a76087a6ac91a1fe8022b411a353b009bd
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-08-27 14:05:34 +0100

    feat: Add thunk to store

--- src/redux/cofigureStore.js
+++ src/redux/cofigureStore.js
@@ -1,11 +1,12 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 import books from './books/bookReducer';
 
 export const reducer = combineReducers({
     books
 });
 
-const store = createStore(reducer,applyMiddleware(logger));
+const store = createStore(reducer,applyMiddleware(thunk, logger));
 
 export default store;
