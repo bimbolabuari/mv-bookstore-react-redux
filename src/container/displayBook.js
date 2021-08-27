67ff785a4ce8f9cf0e3160cd44aa8da635770571
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-08-27 14:07:28 +0100

    feat: Import fetch API to displaybook component

--- src/container/displayBook.js
+++ src/container/displayBook.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from "prop-types";
-import { connect } from 'react-redux';
-import { removeBook } from '../actions/bookActionCreator';
+import { connect, useDispatch } from 'react-redux';
+import { removeBook, getBook } from '../actions/bookActionCreator';
 
 const DisplayBooks = (props) => {
-    // {books, removeBook }
-console.log(props)    
-const {books, removeBook } = props
+const {books, removeBook} = props
+
+
+
+const dispatch = useDispatch();
+const fetchApi = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/BIaRoNTXarfAkaRwuuR5/books/';
+useEffect(() => {
+  const apiBooks = async () => {
+    const fetchBook = await fetch(fetchApi);
+    const books = await fetchBook.json();
+    return dispatch(getBook(books));
+  };
+  apiBooks();
+}, []);
+
     return (
         <ul>
             { books.map(book => <li key={book.id}>
                 <div>
-                    <p>{book.type}</p>
+                    <p>{book.category}</p>
                     <h2>{book.title}</h2>
                     <h3>{book.author}</h3>
                 </div>
